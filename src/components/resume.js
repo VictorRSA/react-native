1cd4d5dad5bac4818ed02e5f04f8b14b2dec39d9
victor nkuna <victor.nkuna@yahoo.com>
2021-01-05 00:12:38 +0200

    add working react

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import A from 'react-native-a'
 import { Grid, Cell } from 'react-mdl';
-import Education from './education';
-import Experience from './experience';
-import Skills from './skills';
+import Education from '../../UI/components/components/education';
+import Experience from '../../UI/components/components/experience';
+import Skills from '../../UI/components/components/skills';
 
 
 class Resume extends Component {
@@ -19,19 +20,22 @@ class Resume extends Component {
                  />
             </div>
 
-            <h2 style={{paddingTop: '2em'}}>Victor</h2>
+            <h2 style={{paddingTop: '2em'}}>Victor Nkuna</h2>
             <h4 style={{color: 'grey'}}>Programmer</h4>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
-            <p>I'm an individual characterised by openness to experience and a dynamic personality which is  epitomized by my intellectual curiosity. I have a keen interest in the financial service industry in its broadest form, specifically financial inclusion using latest technology like block-chain and Artificial Intelligence. </p>
+            <p>.</p>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
             <h5>Address</h5>
-            <p>5 Flammingo Crescent Lansdwane,Cape Town, 7779</p>
+            <p>5 Flammingo Crescent,
+               Nerissa Estate, 
+                Lansdwane,
+                  7780</p>
             <h5>Phone</h5>
-            <p>(+27) 760-170851</p>
+            <p>(+277) 670-170851</p>
             <h5>Email</h5>
             <p>victor.nkuna@yahoo.com</p>
             <h5>Web</h5>
-            <p>https://victor-nkuna-online-portfolio.netlify.app/</p>
+            <p><A>https://loving-ramanujan-c0c2cb.netlify.app/</A></p>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
           </Cell>
           <Cell className="resume-right-col" col={8}>
@@ -42,35 +46,87 @@ class Resume extends Component {
               startYear={2010}
               endYear={2014}
               schoolName="Ngakana Secondary School"
-              schoolDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+              
                />
 
                <Education
-                 startYear={2007}
-                 endYear={2009}
-                 schoolName="My University"
-                 schoolDescription="Bachelor of Commerce specialising in Management Studies [CB001BUS06]  is the most flexible degree offered in the Faculty of Commerce. It gives students the ability to discover what they enjoy during their first and second years of study and make adjustments before their final year. The degree enables this by allowing students to take the same core courses as BBusSc students up to the third-year level.  How this works is if you are a student interested in this degree, you will be prescribed more courses than your peers on the other BCom degrees. In addition to the 18 core courses, you will be required to complete another 9 electives. 
+                 startYear={2015}
+                 endYear={2019}
+                 schoolName="University of Cape Town"
+                 CourseName-Last-Programme="Bcom Management Studies"
+                 ProgrameDescription="Management  Studies  is  the  most  flexible  three-year  degree offered in the Faculty of Commerce. The degree requires students to take a rigorous  core  of  18  courses.  In  addition  to  these  18  core  courses,  students  must  complete another 9 electives from a range of disciplines offered by Commerce and other  faculties.  These  27  courses  make  the  BCom  specialising  in  Management  Studies an intensive choice of study. Management  Studies  is  ideal  for  a  student  who  wants  a  more  personalised  combination of disciplines and the opportunity to tailor the degree to their specific requirements. The combination of academic rigour, breadth and flexibility,
+                  enables students  to  leverage  their  strengths  and  interests"
+                 Completed="No"
+                 Academic-Record="Provided Upon Request"
 
-                 This gives students a chance to discover what they enjoy doing within the School of Management Studies and honing in on that to complete their degree. This also sets the trajectory for their subsequent postgraduate degrees, so it is important that you aren't sure what you want to do after attaining your degree that you seek guidance and introspection during this time. This degree leaves room to do just that. http://www.students.uct.ac.za/students/study/degrees-diplomas/commerce/undergraduate"
-                  Course Name Last Programme="Bcom(Management Studies)"
-                  Completed="No"
                   />
                 <hr style={{borderTop: '3px solid #e22947'}} />
 
               <h2>Experience</h2>
 
-            <Experience
-              startYear={2009}
-              endYear={2012}
-              jobName="First Job"
-              jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-              />
+              <Additional-Certificate
+                Name="Responsive Web Design HTML"
+                Issuing-Organisation="<A>https://www.freecodecamp.org/</A>"
+                Issued={1-2020}
+                Description="Responsive Web Design (HTML, CSS, Flexbox, CSS Grid)"
+                Credential-ID="https://www.freecodecamp.org/certification/fcc276718ef-256c-4c3e-a4c3-d8bfe61859cc/responsive-web-design"
+                />
 
-              <Experience
-                startYear={2012}
+              <Additional-Certificate
+                Name="HTML Course"
+                Issuing-Organisation="<A>https://www.sololearn.com/learning/1014</A>"
+                Issued={23-11-2020}
+                Description="This course  teach ones how to design a web 
+                            page using HTML.Complete a series of hands-on exercises and practice while writing real HTML code."
+                Credential-ID="https://www.sololearn.com/Certificate/1014-16998995/pdf"
+                />
+
+              <Additional-Certificate
+                Name="JavaScript Course"
+                Issuing-Organisation="<A>https://www.sololearn.com/learning/1024</A>"
+                Issued={2-11-2020}
+                Description="Responsive Web Design (HTML, CSS, Flexbox, CSS Grid)"
+                Credential-ID="https://www.sololearn.com/Certificate/1024-16998995/pdf/"
+                />
+
+                
+
+
+            <Experience
+              Organisation="University of Cape Town: Leo Marquard Residence "
+              jobName="Student Residence Librarian"
+              jobDescription="Duties:
+              • Daily inventory management and tracking
+              • Liaising with various stakeholders to discuss initiatives
+              • Collecting student complaints directly related to books"
+              startYear={2016}
+              endYear={2016} 
+              />
+              <hr style={{borderTop: '3px solid #e22947'}} />
+
+              <Additional-Certificate
+                Name="Presentation Skills Course"
+                Issuing-Organisation="<A>https://jenxhr.wordpress.com/services/ </A>"
+                startYear={4-8-2015}
+                endYear={15-9-2015}
+                Description="Professional Presentation and Communication Skills Programs"
+                Credential-ID="https://drive.google.com/file/d/1s451FfWV1YIKxFkJveUwcOL0uO1-K6jh/view?usp=sharing"
+                />
+
+
+              <Volunteering-Experience
+                Name="Social Service"
+                Issuing-Organisation="<A>https://marquardroar.wordpress.com/2016/03/06/residential-life-leo-marquard-hall/</A>"
+                startYear={2016}
                 endYear={2016}
-                jobName="Second Job"
-                jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+                jobName="Outreach Intern"
+                jobDescription="As an Outreach intern, I was assisting with the overall seamless functioning of 24 centres by helping with many of the outreach needs and general office assistance of the organization. Core functions of this position include supporting the
+                Regional Director in stewardship and outreach with event participants, as well as assisting in administrative duties focused on
+                event recruitment and fundraising. Tasks include pulling and analyzing reports from our fundraising platform, prepping event
+                materials and logistics, and communicating with event participants and event sponsors. The intern position exposed me to a nonprofit organization, fundraising, database management, community outreach, and communications.
+                requires a positive attitude, ability to work independently and as a team, and proven writing, editing, and customer service
+                skills.The role required some one who was comfortable on the phone and willing to attend events. "
+                Credential-ID="https://drive.google.com/file/d/1XHwzsqUAITRUxBcYceBP_5o9a7QDup5g/view?usp=sharing"
                 />
               <hr style={{borderTop: '3px solid #e22947'}} />
               <h2>Skills</h2>
@@ -89,6 +145,10 @@ class Resume extends Component {
                     <Skills
                       skill="React"
                       progress={25}
+                      />
+                      <Skills
+                      skill="Python-Flask-Django"
+                      progress={45}
                       />
 
 
